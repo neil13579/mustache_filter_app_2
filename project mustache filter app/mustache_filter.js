a5e8b60cbd195cb8347350b61381aed7cbcdee5c
neil13579 <67318535+neil13579@users.noreply.github.com>
2021-01-31 13:14:47 +0530

    Add files via upload

---
+++ project mustache filter app/mustache_filter.js
@@ -0,0 +1,36 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('posenet is initialized');
+}
+
+function gotPoses(results) {
+
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 45;
+        noseY = results[0].pose.nose.y - 40;
+    }
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+
+}
